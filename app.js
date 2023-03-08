d72441aadb5526a8893d99f0ad67c57860eaf193
marotella <michelle.a.rotella@gmail.com>
2023-03-07 20:16:25 -0500

    Index route

--- app.js
+++ app.js
@@ -2,8 +2,48 @@
 require('dotenv').config();
 const express = require("express")
 const app = express()
+const mongoose = require('mongoose');
+const methodOverride = require("method-override")
 
 
+// Database Connection
+mongoose.connect(process.env.DATABASE_URL);
+
+// Database Connection Error/Success
+// Define callback functions for various events
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Middleware
+//Body parser: Add JSON data from request to the request object
+app.use(express.json())
+// Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"))
+
+//ROUTES and CONTROLLERS
+
+
+//INDEX
+app.get("/project", (req, res)=> {
+    
+    res.send("Index Page")
+    console.log("index route running")
+})
+
+//NEW
+
+//DELTE
+
+//UPDATE
+
+//CREATE
+
+//EDIT
+
+//SHOW
 
 
 //Listener
