0bc44c0535eb643f6861aab86bb56953f214b7db
marotella <michelle.a.rotella@gmail.com>
2023-03-11 11:58:24 -0500

    Can create a new user

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,21 @@ router.get("/register", (req, res)=>{
     res.render("users/register.ejs")
 })
 
-
+router.post("/register", (req, res)=> {
+    const salt = bcrypt.genSaltSync(10)
+    req.body.password = bcrypt.hashSync(req.body.password, salt)
+    console.log(req.body)
+    User.findOne({username: req.body.username}, (err,userExists)=> {
+        if(userExists){
+            res.send("That user name already exists.")
+        }else {
+            User.create(req.body, (err, createdUser)=>{
+                console.log(createdUser)
+                res.redirect("/project")
+            })
+        }
+    })
+})
 
 
 router.get("/signin", (req, res)=>{
